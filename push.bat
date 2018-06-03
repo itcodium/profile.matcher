b4870860e0603cf377cc93399bd5c5ca758c2c82
itcodium <p.haddad.p@gmail.com>
2018-06-02 22:34:29 -0300

    2018-06-02 Se actualiza el chatbot para que procese listas (II).

--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "2018-06-02 Se actualiza el chatbot para que procese listas."
+git commit -m "2018-06-02 Se actualiza el chatbot para que procese listas (II)."
 git push origin chatbot
